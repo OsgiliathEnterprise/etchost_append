0cba988b6507c097f98312a8df41a57425e28e01
Charlie Mordant <cmordant1@gmail.com>
2026-06-07 23:01:33 +0200

    chore: upgrade to uv

---
+++ .ansible/collections/ansible_collections/tcharl/etchost_append/molecule/default/tests/test_default.py
@@ -0,0 +1,30 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file_contains_the_new_entry(host):
+    command = r"""cat /etc/hosts | \
+    grep -Ec \
+    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+poc\.osgiliath\.test'"""
+    cmd = host.run(command)
+    assert int(cmd.stdout) >= 1
+
+
+def test_additional_hostname1_added(host):
+    command = r"""cat /etc/hosts | \
+    grep -Ec '^192\.168\.122\.1\s+idm\.osgiliath\.test$'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_additional_hostname2_added(host):
+    command = r"""cat /etc/hosts | \
+    grep -Ec '^192\.168\.122\.2.+infra\.osgiliath\.test'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_hostname_is_updated(host):
+    command = r"""hostname | \
+    grep -c 'dns.osgiliath.test'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
